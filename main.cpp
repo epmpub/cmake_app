0f1fbac020adabb4d3fdfe22504f5f61dff8a206
Your Name <you@example.com>
2025-08-21 15:22:04 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <fmt/core.h>
+#include <spdlog/spdlog.h>
+
+int main(int, char**){
+    spdlog::info("Hello, World from spdlog!");
+    fmt::print("Hello, World from fmt!\n");
+    return 0;
+}
